d4e63306e6d2764ecc3dcdef58f488510315f248
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-18 20:32:45 +0600

    add project all

--- components/Home/Education.tsx
+++ components/Home/Education.tsx
@@ -1,15 +1,25 @@
+import Image from "next/image";
 
 
 export default function Education() {
    return(
       <div>
-         <div>
-            <h2>Education and Training</h2>
-            <p>My education has been a journey of self-discovery and growth. Here are my educational details.</p>
+         <div className="flex flex-col items-center">
+            <h2 className="text-[30px] font-bold mb-4  ">Education and Training</h2>
+            <p className="text-lg font-medium text-[#9CA3AF] mb-12 ">My education has been a journey of self-discovery and growth. Here are my educational details.</p>
             <div>
-               <div></div>
-               <div></div>
-               <div></div>
+               <div>
+                  <div>
+                     <div>
+                        <Image src="" alt="SSC" />
+                     </div>
+                     <div>
+                        <h3>government tolaram college</h3>
+                        <p>Bander, Narayanganj</p>
+                     </div>
+                  </div>
+                  <div></div>
+               </div>
             </div>
          </div>
       </div>
--- components/Home/Projects.tsx
+++ components/Home/Projects.tsx
@@ -11,26 +11,86 @@ export default function Projects() {
          <div>
             <h2 className="text-[30px] font-bold text-center ">My Best Work</h2>
             <p className="text-xl font-bold text-center text-[#F87171] ">Explore My Projects and work</p>
-            <div>
+            <div className="flex flex-row items-center gap-10">
                <div className="w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
-                  <Image src={AsumGamer} alt="" />
-                  <div>
-                     <Badge color="blue" className="text-white" >In review</Badge>
-                     <Badge color="cyan"><span className="text-white">next</span></Badge>
-                     <Badge color="gray" variant="solid" highContrast>
-                        New
-                     </Badge>
+                  <Image src={AsumGamer} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
+                  <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
+                     <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Node.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Express.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">MongoDB</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Firebase</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">JWT</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Redux</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
                   </div>
-                  <h4></h4>
-                  <p></p>
-                  <div className="flex flex-row items-center gap-6">
-                     <Button color="gray" variant="solid">
-                        Client Repo
+                  <h4 className="text-white text-2xl font-semibold mb-2">Asum Gamer BD</h4>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">A game review website</p>
+                  <div className="flex flex-row items-center gap-3 ">
+                     <Button color="gray" variant="solid" >
+                         <a href="https://github.com/AshrafulPatHan/Chill-Gamer" target="_blank"  className=" cursor-pointer">Client Repo</a>
                      </Button>
                      <Button color="gray" variant="solid">
-                        Server Repo
+                        <a href="https://github.com/AshrafulPatHan/chill-gamer-server" target="_blank"  className=" cursor-pointer">
+                           Server Repo
+                        </a>
                      </Button>
-                     <Button variant="solid">Live Link</Button>
+                     <Button variant="solid">
+                        <a href="https://asum-gamer-bd.vercel.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
+                     </Button>
+                  </div>
+               </div>
+               <div className="w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
+                  <Image src={Techno} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
+                  <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
+                     <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Node.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Express.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">MongoDB</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Firebase</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">JWT</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
+                  </div>
+                  <h4 className="text-white text-2xl font-semibold mb-2">techno blogger</h4>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">A Blogging website</p>
+                  <div className="flex flex-row items-center gap-3 ">
+                     <Button color="gray" variant="solid" >
+                         <a href="https://github.com/AshrafulPatHan/Techno-Bloger" target="_blank"  className=" cursor-pointer">Client Repo</a>
+                     </Button>
+                     <Button color="gray" variant="solid">
+                        <a href="https://github.com/AshrafulPatHan/Techno-Server" target="_blank"  className=" cursor-pointer">
+                           Server Repo
+                        </a>
+                     </Button>
+                     <Button variant="solid">
+                        <a href="https://techno-bloger.web.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
+                     </Button>
+                  </div>
+               </div>
+               <div className="w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
+                  <Image src={chillGamer} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
+                  <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
+                     <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Node.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Express.js</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">MongoDB</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Firebase</span></Badge>
+                     <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
+                  </div>
+                  <h4 className="text-white text-2xl font-semibold mb-2">ChillGamer</h4>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">A game review website</p>
+                  <div className="flex flex-row items-center gap-3 ">
+                     <Button color="gray" variant="solid" >
+                         <a href="https://github.com/AshrafulPatHan/Chill-Gamer" target="_blank"  className=" cursor-pointer">Client Repo</a>
+                     </Button>
+                     <Button color="gray" variant="solid">
+                        <a href="https://github.com/AshrafulPatHan/chill-gamer-server" target="_blank"  className=" cursor-pointer">
+                           Server Repo
+                        </a>
+                     </Button>
+                     <Button variant="solid">
+                        <a href="https://chillgamer-10bf2.web.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
+                     </Button>
                   </div>
                </div>
             </div>
