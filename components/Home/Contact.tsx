b3090c18f97e1e3e2b7cdd71ad1333779cc5c8a9
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-18 21:31:23 +0600

    statick side is complite

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact(){
          <div className="flex flex-col items-center">
             <p className=" text-[16px] text-[#6B7280] ">Contact</p>
             <h2 className="font-bold text-[36px] mb-6 ">CONTACT WITH ME</h2>
-            <div className="flex flex-row items-center justify-between p-4 w-[1122px] bg-black border-2 border-blue-100 text-white rounded-lg ">
+            <div className="flex flex-row items-center gap-20 p-4 w-[900px] bg-black border-2 border-blue-100 text-white rounded-lg ">
                <div className="flex flex-col gap-2 ">
                   <Image src={ContactImg} alt="Contact" className="w-[170px] h-[170px] " />
                   <div className="flex flex-col gap-1">
@@ -17,14 +17,18 @@ export default function Contact(){
                      <p className="text-lg font-semibold">Or, Call: <span className="text-[#725bd6] ">+8801568706037</span></p>
                   </div>
                </div>
-               <div className="flex flex-col ">
+               <div className="flex flex-col gap-3 ">
                   <div>
-                     <input type="text" name="email" placeholder="Write your email address" id="email" />
+                     <input type="text" name="email" placeholder="Write your email address" id="email" 
+                     className="border-[1.5px] p-2 rounded-lg w-[300px]  "
+                     />
                   </div>
                   <div>
-                     <input type="text" name="Message" placeholder="Write your massage" id="Message" />
+                     <input type="text" name="Message" placeholder="Write your massage" id="Message" 
+                     className="border-[1.5px] p-2 rounded-lg w-[300px]  "
+                     />
                   </div>
-                  <button>
+                  <button className="bg-blue-500 p-3 rounded-lg cursor-pointer">
                      Submit
                   </button>
                </div>
