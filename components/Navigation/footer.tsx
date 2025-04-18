570ffe10fe144d0c5d740e68338528fba51644b9
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-18 12:15:08 +0600

    add footer

--- components/Navigation/footer.tsx
+++ components/Navigation/footer.tsx
@@ -1,26 +1,65 @@
-
+import Image from "next/image";
+import facebookLogo  from "@/assets/image/logo/icons8-facebook.svg";
+import twitterLogo  from "@/assets/image/logo/bird.svg";
+import linkedinLogo  from "@/assets/image/logo/icons8-linkedin.svg";
+import GithubLogo  from "@/assets/image/logo/github.png";
 
 export default function Footer() {
    return(
-      <div>
-         <div >
-            <h4>Ashraful Pathan</h4>
-            <div>
+      <div className="font-Cinzel h-auto  p-10 bg-gradient-to-r from-[#302f2f] to-[#0f0f0f]">
+         <div className="flex flex-col items-center gap-4 py-12 text-white">
+            <h4 className="font-bold text-2xl ">Ashraful Pathan</h4>
+            <div className="flex flex-row items-center gap-6 text-xl font-semibold">
                <p>Blog</p>
                <p>projects</p>
                <p>Admin Panel</p>
                <p>code</p>
             </div>
-            <div>
-            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FacebookIcon"><path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"></path></svg>
-            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="TwitterIcon"><path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z"></path></svg>
-            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LinkedInIcon"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="64" height="64" viewBox="0 0 64 64">
-            <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"></path>
-            </svg>
-
+            <div className="flex flex-row items-center gap-3">
+               <a href="https://github.com/AshrafulPatHan" 
+                target="_blank" 
+                className=" "
+                rel="noopener noreferrer" >
+                    <Image
+                    src={GithubLogo}
+                    alt="Facebook link"
+                    width={30}
+                    height={30}
+                    
+                    />
+               </a>
+               <a href="https://www.facebook.com/profile.php?id=100090953234693" 
+                  target="_blank" 
+                  rel="noopener noreferrer" className=" ">
+                     <Image
+                     src={facebookLogo}
+                     alt="Facebook link"
+                     width={30}
+                     height={30}
+                     />
+                  </a>
+               <a href="https://x.com/AshrafulPa25233" 
+                  target="_blank" 
+                  rel="noopener noreferrer" className="">
+                     <Image
+                     src={twitterLogo}
+                     alt="Facebook link"
+                     width={30}
+                     height={30}
+                     />
+                  </a>
+               <a href="https://www.linkedin.com/in/ashraful-pathan" 
+                  target="_blank" 
+                  rel="noopener noreferrer" className=" ">
+                     <Image
+                     src={linkedinLogo}
+                     alt="Facebook link"
+                     width={30}
+                     height={30}
+                     />
+                  </a>
             </div>
-            © 2025 Ashraful Pathan. All rights reserved.
+            <p className="text-sm mt-3 " > © 2025 Ashraful Pathan. All rights reserved.</p>
          </div>
       </div>
    )
