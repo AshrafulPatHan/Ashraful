9d762e665996e7a50c70022b343e7c7a1406aacf
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-19 16:13:25 +0600

    add programming hreo verview

--- components/Home/Education.tsx
+++ components/Home/Education.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
 import tolaram from "@/assets/image/tolaram.png";
+import Programming from "@/assets/image/programing-hero.jpeg";
+import * as motion from "motion/react-client"
 
 
 export default function Education() {
    return(
-      <div className="py-16  bg-gradient-to-b from-gray-900 to-gray-900 text-white">
+      <div className="py-20  bg-gradient-to-b from-gray-900 to-gray-900 text-white">
          <div className="flex flex-col items-center">
             <h2 className="text-[30px] font-bold mb-1  ">Education and Training</h2>
             <p className="text-lg font-medium text-[#9CA3AF] mb-12  text-center">My education has been a journey of self-discovery and growth. Here are my educational details.</p>
-            <div>
-               <div className="flex flex-col gap-4 w-[100vw] lg:w-[900px] p-6 bg-[#030712] text-white rounded-lg  ">
+            <div className="flex flex-col items-center gap-14">
+               <motion.div
+               whileHover={{ y: -8 }}
+               transition={{ type: "spring", stiffness: 200, damping: 15 }}
+               className="flex flex-col gap-4 w-[100vw] lg:w-[900px] p-6 bg-[#030712] text-white rounded-lg 
+               hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
                   <div className="flex flex-row items-center gap-4">
                      <div>
                         <Image src={tolaram} alt="Hsc" className="w-[74px] h-[74px]  " />
@@ -22,11 +28,37 @@ export default function Education() {
                      </div>
                   </div>
                   <div className="flex flex-col gap-2">
-                     <h4>Grade: Pending GPA</h4>
+                     <h4 className="text-green-400">Grade: Pending GPA</h4>
                      <p>Currently pursuing a Diploma in Engineering in Computer Science and Technology at Bhola Polytechnic Institute. I have successfully completed 6 out of 8 semesters, gaining expertise in courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Computer Networks, Microcontroller Programming, and Sensor Technologies. Additionally, I am proficient in tools like MS Word and Excel.</p>
                      <p>As an active member of the Target Programmer Bhola Polytechnic Institute, I collaborate on innovative projects with a team of skilled developers, enhancing my technical and teamwork skills.</p>
                   </div>
-               </div>
+               </motion.div>
+               <motion.div
+               whileHover={{ y: -8 }}
+               transition={{ type: "spring", stiffness: 200, damping: 15 }}
+               className="flex flex-col gap-4 w-[100vw] lg:w-[900px] p-6 bg-[#030712] text-white rounded-lg 
+               hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
+                  <div className="flex flex-row items-center gap-4">
+                     <div>
+                        <Image src={Programming} alt="Hsc" className="w-[74px] h-[74px]  " />
+                     </div>
+                     <div>
+                        <h3 className="font-bold text-[20px] ">programming hero</h3>
+                        <p className="font-bold text-[18px] ">programming-hero.com/</p>
+                        <p className="font-bold text-[16px] text-[#818CF8] ">TRAINING</p>
+                        <p className="text-[14px] text-[#6B7280] ">2024-2025</p>
+                     </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                     <h4 className="text-green-400">course is completed with certificate</h4>
+                     <p>
+                     I found an amazing course on Programming Hero (Batch 10, 2024-2025). I learned React.js, Next.js, JavaScript, HTML, CSS, and Tailwind CSS. The course is really great, and the mentor, Jhankar Mahbub, is an excellent trainer. He explains everything clearly, making complex topics easy to understand. The support team is also fantastic, offering 24/7 assistance whenever needed. This course is a perfect choice for anyone who wants to dive into web development with hands-on experience!
+                     </p>
+                     <p>
+                        I give the course 5 ster .I learn many thing from this course
+                     </p>
+                  </div>
+               </motion.div>
             </div>
          </div>
       </div>
--- components/Home/Projects.tsx
+++ components/Home/Projects.tsx
@@ -3,7 +3,7 @@ import AsumGamer from "@/assets/image/project/asumGamer.png";
 import chillGamer from "@/assets/image/project/chill.png";
 import Techno from "@/assets/image/project/Techno.png";
 import { Badge, Button } from "@radix-ui/themes";
-
+import * as motion from "motion/react-client"
 
 export default function Projects() {
    return(
@@ -13,7 +13,11 @@ export default function Projects() {
             <h2 className="text-[30px] font-bold text-center ">My Best Work</h2>
             <p className="text-xl font-bold text-center text-[#F87171] mb-6 ">Explore My Projects and work</p>
             <div className="flex flex-col xl:flex-row items-center gap-10">
-               <div className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
+               <motion.div 
+                whileHover={{ y: -8 }}
+                transition={{ type: "spring", stiffness: 200, damping: 15 }}
+               className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg 
+               hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
                   <Image src={AsumGamer} alt="image" className=" w-[362px] h-[196px] rounded-lg object-cover  " />
                   <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
                      <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
@@ -26,7 +30,9 @@ export default function Projects() {
                      <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
                   </div>
                   <h4 className="text-white text-2xl font-semibold mb-2">Asum Gamer BD</h4>
-                  <p className="text-[#D1D5DB] text-sm mb-4 ">A game review website</p>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">
+                     Asum Gamer BD is a dynamic and interactive game review platform built using React.js, Node.js, Express.js, MongoDB, Firebase, JWT, Redux, and Tailwind CSS. This website is designed for gamers by a gamer — giving you a space to share your thoughts, discover new games, and connect with fellow gamers.
+                  </p>
                   <div className="flex flex-row items-center gap-3  ">
                      <Button color="gray" variant="solid" >
                          <a href="https://github.com/AshrafulPatHan/Chill-Gamer" target="_blank"  className=" cursor-pointer">Client Repo</a>
@@ -40,8 +46,12 @@ export default function Projects() {
                         <a href="https://asum-gamer-bd.vercel.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
                      </Button>
                   </div>
-               </div>
-               <div className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
+               </motion.div>
+               <motion.div 
+               whileHover={{ y: -8 }}
+               transition={{ type: "spring", stiffness: 200, damping: 15 }}
+               className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  
+               hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
                   <Image src={Techno} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
                   <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
                      <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
@@ -53,7 +63,9 @@ export default function Projects() {
                      <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
                   </div>
                   <h4 className="text-white text-2xl font-semibold mb-2">techno blogger</h4>
-                  <p className="text-[#D1D5DB] text-sm mb-4 ">A Blogging website</p>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">
+                  Techno Blogger is a modern, full-stack blogging platform built using React.js, Node.js, Express.js, MongoDB, Firebase, JWT, Redux, and Tailwind CSS. Designed with a fully responsive UI, it works seamlessly on any device, giving users a smooth and engaging experience wherever they are.
+                  </p>
                   <div className="flex flex-row items-center gap-3 ">
                      <Button color="gray" variant="solid" >
                          <a href="https://github.com/AshrafulPatHan/Techno-Bloger" target="_blank"  className=" cursor-pointer">Client Repo</a>
@@ -67,8 +79,12 @@ export default function Projects() {
                         <a href="https://techno-bloger.web.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
                      </Button>
                   </div>
-               </div>
-               <div className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg  ">
+               </motion.div>
+               <motion.div 
+               whileHover={{ y: -8 }}
+               transition={{ type: "spring", stiffness: 200, damping: 15 }}
+               className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg 
+               hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
                   <Image src={chillGamer} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
                   <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
                      <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
@@ -79,7 +95,9 @@ export default function Projects() {
                      <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
                   </div>
                   <h4 className="text-white text-2xl font-semibold mb-2">ChillGamer</h4>
-                  <p className="text-[#D1D5DB] text-sm mb-4 ">A game review website</p>
+                  <p className="text-[#D1D5DB] text-sm mb-4 ">
+                     Chill Gamer is a dynamic and interactive game review platform built using React.js, Node.js, Express.js, MongoDB, Firebase, JWT, Redux, and Tailwind CSS. This website is designed for gamers by a gamer — giving you a space to share your thoughts, discover new games, and connect with fellow gamers.   
+                  </p>
                   <div className="flex flex-row items-center gap-3 ">
                      <Button color="gray" variant="solid" >
                          <a href="https://github.com/AshrafulPatHan/Chill-Gamer" target="_blank"  className=" cursor-pointer">Client Repo</a>
@@ -93,7 +111,7 @@ export default function Projects() {
                         <a href="https://chillgamer-10bf2.web.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
                      </Button>
                   </div>
-               </div>
+               </motion.div>
             </div>
          </div>
       </div>
