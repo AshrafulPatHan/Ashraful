b56cc40beeb2d3108ee548c6929532f18489a15e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-19 11:47:54 +0600

    add /

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Logo from "@/assets/image/logo.svg";
 import snow from "@/assets/image/icons/snowflake.png";
 import { Button, DropdownMenu } from "@radix-ui/themes";
+import Link from "next/link";
 
 
 
@@ -9,10 +10,10 @@ export default function Navbar() {
    return(
       <div className="bg-gradient-to-r from-black via-gray-900 to-gray-950 backdrop-blur-lg sticky top-0 z-50 px-8 py-2 ">
          <div className="flex flex-row items-center justify-between  text-white ">
-            <div className="flex flex-row items-center gap-2">
+            <Link href='/' className="flex flex-row items-center gap-2 cursor-pointer">
                <Image src={Logo} alt="logo" width={40}  />
                <h3 className="text-[5vw] sm:text-xl font-bold ">Ashraful pathan</h3>
-            </div>
+            </Link>
             <div className="hidden lg:flex lg:flex-row items-center gap-4">
                <p className="text-lg font-bold transition-all duration-500 hover:text-blue-600 cursor-pointer ">HOME</p>
                <p className="text-lg font-bold transition-all duration-500 hover:text-blue-600 cursor-pointer ">SKILLS</p>
