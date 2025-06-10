f70d238680dfaf8600a9c1edb1e96bbab58862d7
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-10 11:40:52 +0600

    update

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -1,7 +1,8 @@
 "use client"
 import Image from "next/image";
 import AsumGamer from "@/assets/image/project/asumGamer.png";
-import chillGamer from "@/assets/image/project/chill.png";
+import chillGamer from "@/assets/image/project/chill.png"; 
+import School from "@/assets/image/project/school.png"; 
 import Techno from "@/assets/image/project/Techno.png";
 import { Badge, Button } from "@radix-ui/themes";
 import * as motion from "motion/react-client"
@@ -110,30 +111,31 @@ export default function Projects() {
                   transition={{ type: "spring", stiffness: 200, damping: 15 }}
                   className="w-[100vw] sm:w-[394px] h-[536px] p-4 bg-[#1F2937] rounded-lg 
                   hover:shadow-[0_10px_30px_rgba(0,0,0,0.8)] transition-shadow duration-300 ">
-                     <Image src={chillGamer} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
+                     <Image src={School} alt="image" className="w-[362px] h-[196px] rounded-lg object-cover  " />
                      <div className=" grid grid-cols-4 gap-2 mt-3 mb-4">
                         <Badge size="2" color="cyan"><span className="text-white">React.js</span></Badge>
                         <Badge size="2" color="cyan"><span className="text-white">Node.js</span></Badge>
                         <Badge size="2" color="cyan"><span className="text-white">Express.js</span></Badge>
                         <Badge size="2" color="cyan"><span className="text-white">MongoDB</span></Badge>
-                        <Badge size="2" color="cyan"><span className="text-white">Firebase</span></Badge>
+                        <Badge size="2" color="cyan"><span className="text-white">JWT</span></Badge>
+                        <Badge size="2" color="cyan"><span className="text-white">Gsap</span></Badge>
                         <Badge size="2" color="cyan"><span className="text-white">Tailwind CSS</span></Badge>
                      </div>
-                     <h4 className="text-white text-2xl font-semibold mb-2">ChillGamer</h4>
+                     <h4 className="text-white text-2xl font-semibold mb-2">School</h4>
                      <p className="text-[#D1D5DB] text-sm mb-4 ">
-                        Chill Gamer is a dynamic and interactive game review platform built using React.js, Node.js, Express.js, MongoDB, Firebase, JWT, Redux, and Tailwind CSS. This website is designed for gamers by a gamer — giving you a space to share your thoughts, discover new games, and connect with fellow gamers.   
+                        Welcome to Kuripara High School's official website, proudly built by a former student using React.js, Node.js, Express.js, MongoDB, JWT, GSAP, and Tailwind CSS. This site helps students, teachers, and guardians stay updated, connected, and informed about school news, notices, and achievements.
                      </p>
                      <div className="flex flex-row items-center gap-3 ">
                         <Button color="gray" variant="solid" >
-                           <a href="https://github.com/AshrafulPatHan/Chill-Gamer" target="_blank"  className=" cursor-pointer">Client Repo</a>
+                           <a href="https://github.com/AshrafulPatHan/kuripara-high-school" target="_blank"  className=" cursor-pointer">Client Repo</a>
                         </Button>
                         <Button color="gray" variant="solid">
-                           <a href="https://github.com/AshrafulPatHan/chill-gamer-server" target="_blank"  className=" cursor-pointer">
+                           <a href="https://github.com/AshrafulPatHan/not_for_publick" target="_blank"  className=" cursor-pointer">
                               Server Repo
                            </a>
                         </Button>
                         <Button variant="solid">
-                           <a href="https://chillgamer-10bf2.web.app/" target="_blank"  className=" cursor-pointer">Live Link</a>
+                           <a href="https://kuripara-school.ashraful.top/" target="_blank"  className=" cursor-pointer">Live Link</a>
                         </Button>
                      </div>
                   </motion.div>
